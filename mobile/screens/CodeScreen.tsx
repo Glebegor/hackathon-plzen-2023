6c6ef9e06c0d67088e8df10f0c821904318aef94
thioaethxr <thioaethxr@proton.me>
2023-12-08 20:33:51 +0100

    Mobile: Adding QR reading functionality

--- mobile/screens/CodeScreen.tsx
+++ mobile/screens/CodeScreen.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import { SafeAreaView, Text, Alert } from 'react-native';
+import { RNCamera } from 'react-native-camera';
+import QRCodeScanner from 'react-native-qrcode-scanner';
 
 import { Icon } from '../components/elements/Icon';
 
 import { useStyles } from '../hooks/style';
 
+import type { BarCodeReadEvent } from 'react-native-camera';
+
 export const CodeScreen: React.FC = () => {
   const { appStyles } = useStyles();
 
+  const onScan = (e: BarCodeReadEvent) => {
+    Alert.alert(e.data);
+  };
   return (
     <SafeAreaView style={appStyles.wrapper}>
       <Icon name="qrcode" />
       <Text style={appStyles.text}>Scan</Text>
+      <QRCodeScanner
+        onRead={onScan}
+        flashMode={RNCamera.Constants.FlashMode.torch}
+      />
     </SafeAreaView>
   );
 };
