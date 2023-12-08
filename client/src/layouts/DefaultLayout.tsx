c3ee7548310e2a7a08fec357947170d9dad3794b
thioaethxr <thioaethxr@proton.me>
2023-12-08 12:53:54 +0100

    FE: Creating the Page Navigation block

--- client/src/layouts/DefaultLayout.tsx
+++ client/src/layouts/DefaultLayout.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import { PageNavigation } from '../components/blocks/PageNavigation';
+
 interface Props {
   children: React.ReactNode;
 }
 
 export const DefaultLayout: React.FC<Props> = ({ children }) => (
-  <React.Fragment>{children}</React.Fragment>
+  <React.Fragment>
+    <PageNavigation />
+    {children}
+  </React.Fragment>
 );
