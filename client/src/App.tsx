5af2ae197ffe181b85e8ee08120a500398489981
thioaethxr <thioaethxr@proton.me>
2023-12-08 12:51:36 +0100

    FE: Creating the 404 page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import { HomePage } from './pages/HomePage';
+import { NotFoundPage } from './pages/NotFoundPage';
 
 import { AppRoute } from './utils/route';
 
 export const App: React.FC = () => (
   <Routes>
     <Route path={AppRoute.HOME} element={<HomePage />} />
+    <Route path={AppRoute.NOT_FOUND} element={<NotFoundPage />} />
   </Routes>
 );
---
+++ client/src/pages/NotFoundPage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const NotFoundPage: React.FC = () => (
+  <React.Fragment>
+    <h1>Error 404</h1>
+    <p>Page not found.</p>
+  </React.Fragment>
+);
