c5931ef5192a4b4f2ced21e8b8920d04d68de28d
thioaethxr <thioaethxr@proton.me>
2023-12-08 13:03:24 +0100

    FE: Scalable font size

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { App } from './App.tsx';
 
 import '@styles/reset.scss';
+import '@styles/main.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
