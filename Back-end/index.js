3ca7bbcfe35ae24865fb70333c474b979f7229e0
Hlib Arseniuk <arseniukh@spseplzen.cz>
2023-12-08 13:21:37 +0100

    Created standart routers of auth api.

--- Back-end/index.js
+++ Back-end/index.js
@@ -1,8 +1,24 @@
 const express = require('express');
 
+// Configs
 const config = require('config-yml').load('config/configs.yml')
 console.log(config)
+
+// Application setup
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+// Handlers
+const authRoute = "/auth/";
+const homeController = require('./controllers/LoginController');
+app.use(authRoute + "login", homeController);
+
+const homeController = require('./controllers/RegisterController');
+app.use(authRoute + "register", homeController);
+
+const homeController = require('./controllers/VerifyController');
+app.use(authRoute + "verify", homeController);
+
+
 
 app.listen(PORT, console.log(`Listening on port: ${PORT}`));
